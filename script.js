e26638ec17ba7d4cb1177ee80b59879fa055aeda
Pano Skylakis <pano_skylakis@outlook.com>
2019-05-15 20:00:32 +1200

    Refactor and add To-Do
    Clearly outlined what still needs to be done in order of
    priorities

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 let entries = []
 let temp = ''
 let display = document.getElementById('display')
-// target button and add event listener  *********
-// turn value of button to temp          *********
-// function makeNum() {make button number}  **********
-// function clearEntry() {temp = ''} 
-// function clearAll (sets temp = '' && entries = [])
-// function total - assign each button to correct calculation, entries.push(total); must calc floats;
-// function storeTemp(button) - 
+
+
+//TO DO
+
+//   -JS-
+// pressing '=' multiple times should evaluate using the last operator && number given
+//   eg. 5+5=10 =(5+10) =(5+15)
+// input should only contain X numbers, not infinite
+// % should work correctly (not actually sure)
+// should display 'entries' above 'temp' in smaller font
+// should calculate correctly based on priorities (5+5+(5x5))
+
+// JS STRETCH
+//  add button sounds
+//  create functional widget to add to any website
+//  widget pops/stretches calculator up into centre
+//  X sends back to widget
+//  typing '80085' or '8008135' and pressing '=' shoots confetti
+
+// -CSS-
+// change input direction from left>right to right<left
+// rounded edges
+// no distinct buttons, only button:hover{color:orange}
+// second input for -entries- (smaller font)
+// find matching font style
+// create widget
 
 document.addEventListener('click', buttonValue)
 
@@ -20,7 +39,7 @@ function buttonValue() {
   } else if (button === 'AC') {
         clearAll();
   } else if (button === '=') {
-        total();
+        replaceOperator();
   } else {
         removeOperator();
   }
@@ -39,78 +58,86 @@ function makeNum(button) {
 }
 
 
-
 function clearAll() {
-  button = event.target.value;
-  if (button === 'AC') {
-    entries = [];
-    display.value = (temp = '');
+    button = event.target.value;
+    if (button === 'AC') {
+        entries = [];
+        display.value = (temp = '');
   }
 }
-
 
 
 function clearTemp() {
-  button = event.target.value;
-  if (button === 'CE') {
-    temp = '';
-    display.value = temp;
-  }
+    button = event.target.value;
+    if (button === 'CE') {
+        temp = '';
+        display.value = temp;
+  } 
 }
 
 
-
-function total() {
-  let total = 0;
-  button = event.target.value;
-  for (let i = 0; i < entries.length; i++) {
-    if (entries[i] === 'X') {
-      entries[i] = '*';
+//replace 'X' and '÷' with correct JS operators && evaluate
+function replaceOperator() {
+    button = event.target.value;
+    for (let i = 0; i < entries.length; i++) {
+        if (entries[i] === 'X') {
+            entries[i] = '*';
+        }
+        if (entries[i] === '÷') {
+            entries[i] = '/'
+        }
     }
-    if (entries[i] === '÷') {
-      entries[i] = '/'
-    }
+    if (temp != '') {
+        entries.push(temp);
   }
-  entries.push(temp);
-  entries = entries.join(' ');
-  total = eval(entries);
-  display.value = total;
-  console.log(total);
+  evaluate();
 }
 
 
+function evaluate() {
+    let total = 0; 
+    if (temp = '') {
+        return total += evaluate;
+    } else {
+        entries = entries.join(' ');
+        total = eval(entries);
+    }
+    display.value = total;
+    console.log(total);
+}
 
+
+//push temp to entries when operator is pressed
 function storeTemp() {
   button = event.target.value;
   switch (true) {
-      case(button === '+'):
-        entries.push(temp, '+');
-        temp = '';
-        display.value = temp;
-        break;
-      case(button === '-'):
-        entries.push(temp, '-')
-        temp = '';
-        display.value = temp;
-        break;
-      case(button === 'X'):
-        entries.push(temp, 'X')
-        temp = '';
-        display.value = temp;
-        break;
-      case(button === '%'):
-        entries.push(temp, '%')
-        temp = '';
-        display.value = temp;
-        break;
-      case(button === '÷'):
-        entries.push(temp, '÷')
-        temp = '';
-        display.value = temp;
-        break;
-  }
+        case(button === '+'):
+            entries.push(temp, '+');
+            temp = '';
+            display.value = temp;
+            break;
+        case(button === '-'):
+            entries.push(temp, '-')
+            temp = '';
+            display.value = temp;
+            break;
+        case(button === 'X'):
+            entries.push(temp, 'X')
+            temp = '';
+            display.value = temp;
+            break;
+        case(button === '%'):
+            entries.push(temp, '%')
+            temp = '';
+            display.value = temp;
+            break;
+        case(button === '÷'):
+            entries.push(temp, '÷')
+            temp = '';
+            display.value = temp;
+            break;
+    }
 }
-
 
 
 //if entries[-1] === button && temp = '' do nothing
@@ -119,7 +146,7 @@ function removeOperator() {
     button = event.target.value;
     switch (true) {
         case(entries[entries.length-1] === button && temp === ''):
-          return;
+            return;
         case(entries[entries.length-1] != button && temp === ''):
             entries.splice(-1, 1);
             entries.push(button);
